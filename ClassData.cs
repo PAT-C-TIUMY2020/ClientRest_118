078870d5cfba43de6fb509ab6d38a4ede708efcc
Abdul eriawan nahar <abdul.eriawan.ft18@mail.umy.ac.id>
2020-12-21 20:36:19 +0700

    Add project files.

---
+++ ClassData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRest_20180140118_Abdul_Eriawan_Nahar
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
